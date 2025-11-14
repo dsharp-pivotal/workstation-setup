bd23ef85234e0052c66e0e613f771868e46b8fdc
David Sharp <david.sharp@ditto.com>
2025-11-14 11:24:11 -0800

    Disable accents popup on keyboard long-press

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -23,6 +23,9 @@ killall Dock
 # fast key repeat rate, requires reboot to take effect
 defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
 defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
+# Disable accents popup on long-press
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
